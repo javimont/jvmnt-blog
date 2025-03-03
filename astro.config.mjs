fff6674e4cc299774d399a891e163f8db1f485aa
Javi Mont <jmonterohoyos@gmail.com>
2025-03-03 17:56:50 +0100

    add: iconos "/bits"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,22 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.netlify.jvmnt.com",
-	integrations: [
-		mdx(),
-		sitemap({
-			changefreq: "daily",
-			priority: 1,
-			lastmod: new Date().toISOString().split("T")[0],
-		}),
-		tailwind(),
-	],
-	output: "server",
-	adapter: cloudflare(),
+  site: "https://www.netlify.jvmnt.com",
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: "daily",
+      priority: 1,
+      lastmod: new Date().toISOString().split("T")[0],
+    }),
+    tailwind(),
+    icon()  // Añadido el integration de icon aquí
+  ],
+  output: "server",
+  adapter: cloudflare(),
 });
